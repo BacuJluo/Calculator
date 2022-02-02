875527c345f6307731a35192bcd3f7c4b5f03837
BacuJluo <78647116+BacuJluo@users.noreply.github.com>
2022-02-02 22:25:32 +0300

    add onSaveInstance and onRestoreInstance

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putParcelable("calculator", calculator);
+        outState.putParcelable("calculator",calculator);
     }
 
     @Override
